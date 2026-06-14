e063def9ce61c6c81bb01fb902ccd7e3c56a15c8
dudaselvatti <dudaselvatti@hotmail.com>
2026-06-14 11:20:34 -0300

    feat: implement accordion feature for newly entries and enhance styling

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     
+    // 1. LÓGICA DA TELA INICIAL (Abertura)
     const intro = document.getElementById('intro-screen');
-
     intro.addEventListener('click', () => {
         document.body.classList.add('revealed');
-        console.log("Clique detectado, revelando conteúdo...");
+    });
+
+    // 2. LÓGICA DOS TÓPICOS EXPANSÍVEIS (Accordion)
+    const headers = document.querySelectorAll('.accordion-header');
+    
+    headers.forEach(header => {
+        header.addEventListener('click', () => {
+            const entry = header.parentElement;
+            
+            // Alterna a classe 'active' no dia que foi clicado
+            entry.classList.toggle('active');
+        });
     });
 });
